14c2150d19cbb94fd6c794c276d18eef99224fee
Max Katkalov <makskatkalov@gmail.com>
2023-10-01 00:54:59 +0300

    Initial project commit
    1. Django installed and configured.
    2. This commit introduces the following Django models for the staff management system:
    
    1. `Company` model:
       - Fields: `name`, `foundation_date`, `created_at_staff`, `company_staff_size`, and `description`.
       - Choices for `company_staff_size`.
       - Represents company information such as name, founding date, and size.
    
    2. `Office` model:
       - Fields: `name`, `city`, `country`, `address`, `workspaces`, and `description`.
       - Represents office locations with details like name, location, and available workspaces.
    
    3. `Department` model:
       - Fields: `name`, `owner`, and `company`.
       - Defines departments within the company, with an owner and a reference to the company.
    
    4. `Position` model:
       - Fields: `name` and `department`.
       - Represents job positions with a unique name within a department.
    
    5. `StaffUser` model (extends `AbstractUser`):
       - Fields: `owner`, `department`, `position`, `reporting_to`, `salary`, `office`, `fire_date`,
    `retirement_date`, and `last_salary_review`.
       - Defines user profiles with various attributes including department, position, and reporting hierarchy.

---
+++ staff_app/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import (
+    Company,
+    Office,
+    Department,
+    Position,
+    StaffUser
+)
+
+admin.site.register(Company)
+admin.site.register(Office)
+admin.site.register(Department)
+admin.site.register(Position)
+admin.site.register(StaffUser)
---
+++ staff_us_prjct/settings.py
@@ -0,0 +1,85 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = "django-insecure-m9@d&-&a+@c5t$&3x3*pw2e(=^+@!#=ao9(_i2796!488gk7hq"
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "staff_app.apps.StaffAppConfig"
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "staff_us_prjct.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "staff_us_prjct.wsgi.application"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+AUTH_USER_MODEL = "staff_app.StaffUser"
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "UTC"
+
+USE_I18N = True
+
+USE_TZ = True
+
+STATIC_URL = "static/"
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
