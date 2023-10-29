ed4e4d96f57031caa324007890953990662a52da
Max Katkalov <makskatkalov@gmail.com>
2023-10-29 17:18:09 +0200

    fix N+1 for DepartmentListView

--- staff_app/views.py
+++ staff_app/views.py
@@ -164,7 +164,9 @@ class DepartmentListView(ListView):
     paginate_by = 3
 
     def get_queryset(self):
-        return self.model.objects.filter(company__pk=self.kwargs["pk"])
+        return self.model.objects.select_related(
+            "company"
+        ).filter(company__pk=self.kwargs["pk"])
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
